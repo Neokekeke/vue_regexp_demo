8eeafa48e7040c1018f17e86e9bb51eef365bb9a
Neo <neo_233@163.com>
2018-01-29 21:02:27 +0800

    edit router pages

--- src/components/reg1.vue
+++ src/components/reg1.vue
@@ -5,10 +5,18 @@
   <div class="loginReg">
 
 
-    <input type="text" autofocus="autofocus" v-model="username"/>
-    <input type="password"/>
+    <div class="text">
+      <input type="text" autofocus="autofocus" v-model="username"/>
+    </div>
 
-    登录校验的内容
+    <div class="text">
+      <input type="password"/>
+    </div>
+
+    <div class="content">
+      登录校验的内容
+    </div>
+
   </div>
 </template>
 
@@ -35,7 +43,27 @@ export default {
 
   .loginReg{
     width: 700px;
+    margin: 30px auto;
+  }
+
+  .text{
+    width: 390px;
+    height: 45px;
+    margin: 0 auto;
+  }
+
+  input{
+    display: block;
+    margin: 10px 0 10px 0;
+    width: 100%;
+    height: 100%;
+    font-size: 30px;
+  }
+
+  .content{
     margin: 30px 0 0 0;
+    font-size: 25px;
+    font-weight: 600;
   }
 
 </style>
